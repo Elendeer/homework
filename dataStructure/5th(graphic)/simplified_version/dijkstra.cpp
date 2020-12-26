a279a86e5680c43028803eda4c49c6ea697cb6df
Elendeer <976534093@qq.com>
2020-12-26 20:33:46 +0800

    Finish 5th in DS

--- dataStructure/5th(graphic)/simplified_version/dijkstra.cpp
+++ dataStructure/5th(graphic)/simplified_version/dijkstra.cpp
@@ -2,12 +2,13 @@
  * @Author       : Daniel_Elendeer
  * @Date         : 2020-12-22 20:14:10
  * @LastEditors  : Daniel_Elendeer
- * @LastEditTime : 2020-12-22 21:09:05
+ * @LastEditTime : 2020-12-23 18:27:39
  * @Description  :
 *********************************************/
 #include <iostream>
 #include <algorithm>
 #include <cstring>
+#include <vector>
 
 using namespace std;
 
@@ -17,6 +18,8 @@ int n, m;
 
 int dist[N];
 bool st[N];
+
+vector<int> path;
 
 int dijkstra() {
     memset(dist, 0x3f, sizeof dist);
@@ -28,11 +31,13 @@ int dijkstra() {
             if (!st[j] && (t == -1 || dist[j] < dist[t]))
                 t = j;
 
+        path.push_back(t); // 储存路径
         st[t] = true;
 
         for (int j = 1; j <= n; ++ j )
             dist[j] = min(dist[j], dist[t] + g[t][j]);
     }
+    path.push_back(n);
 
     if (dist[n] == 0x3f3f3f3f) return -1;
     return dist[n];
@@ -42,14 +47,20 @@ int dijkstra() {
 int main() {
     memset(g, 0x3f, sizeof g );
 
-    cin >> n >> m;
-    int a, b, c;
-    while (m -- ) {
-        scanf("%d%d%d", &a, &b, &c);
-        g[a][b] = min(g[a][b], c);
-    }
+    n = 3; // 汇点为节点3
+    g[1][2] = 2;
+    g[2][3] = 1;
+    g[1][3] = 4;
 
-    cout << dijkstra() << endl;
+    int len = dijkstra();
+    cout << "Path : " << endl;
+    for (int i : path) {
+        cout << i << " ";
+    }
+    cout << endl;
+
+    cout << "Length : " << len << endl;
+
 
     return 0;
 }
