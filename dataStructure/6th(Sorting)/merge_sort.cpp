bed5e49c9a0e95519b33f0948aeb15ddab89756b
Elendeer <976534093@qq.com>
2020-12-29 12:03:42 +0800

    Start 6th(Sorting) in DS

---
+++ dataStructure/6th(Sorting)/merge_sort.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+const int N = 1e5 + 10;
+int q[N];
+int temp[N];
+
+void merge_sort(int l, int r, int q[]) {
+	if (l >= r) return ;
+
+	int mid = (l + r) >> 1;
+	merge_sort(l, mid, q);
+	merge_sort(mid + 1, r, q);
+
+	int i = l, j = mid + 1;
+	int k = 0;
+	while (i <= mid && j <= r) {
+		if (q[i] < q[j]) temp[k ++ ] = q[i ++ ];
+		else temp[k ++ ] = q[j ++ ];
+	}
+
+	while (i <= mid) temp[k ++ ] = q[i ++ ];
+	while (j <= r) temp[k ++ ] = q[j ++ ];
+
+	for (int i = l, j = 0; j < k; ++ i, ++ j) {
+		q[i] = temp[j];
+	}
+}
+
+int main () {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++ i ) {
+		scanf("%d", &q[i]);
+	}
+
+	merge_sort(0, n - 1, q);
+
+	for (int i = 0; i < n; ++ i ) {
+		printf("%d ", q[i]);
+	}
+
+	return 0;
+}
---
+++ dataStructure/6th(Sorting)/quick_sort.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+const int N = 1e5 + 10;
+int q[N];
+
+void quick_sort(int l, int r, int q[]) {
+	if (l >= r) return ;
+
+	int i = l - 1, j = r + 1, x = q[(l + r) >> 1];
+
+	while (i < j) {
+		do ++ i; while (q[i] < x);
+		do -- j; while (q[i] > x);
+
+		if (i < j) {
+			swap(q[i], q[j]);
+		}
+	}
+
+	quick_sort(l, j, q);
+	quick_sort(j + 1, r, q);
+}
+
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++ i ) {
+		scanf("%d", &q[i]);
+	}
+
+	quick_sort(0, n - 1, q);
+
+	for (int i = 0; i < n; ++ i ) {
+		cout << q[i] << " ";
+	}
+	
+	return 0;
+}
